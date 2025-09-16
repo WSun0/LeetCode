d3962b6b4d7a39d674a223b79391e62eeb4f6f03
WSun0 <sun.wil@northeastern.edu>
2025-09-16 14:31:05 -0400

    Two Sum

---
+++ 0001.java
@@ -0,0 +1,14 @@
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> differences = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            if(differences.containsKey(target - nums[i])) {
+                return new int[]{i, differences.get(target - nums[i])};
+            } else {
+                differences.put(nums[i], i);
+            }
+        }
+        return new int[]{1, 2};
+    }
+}
